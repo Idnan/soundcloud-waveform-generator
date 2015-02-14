92598cb538cdc46b0a5018f2d0dc96cd3f5ca869
adnan <mahradnan@hotmail.com>
2015-02-14 14:38:14 +0500

    updated default setting options

--- soundcloud-waveform.js
+++ soundcloud-waveform.js
@@ -7,41 +7,41 @@ Array.prototype.max = function() {
 var SoundCloudWaveform = {
 
 	settings : {
-		bar: {
-			width: 3,
-			gap : 0.2
-		},
-		wave: {
-			color: "#666"
-		},
-		audioContext: new AudioContext(),
-		download: false
+		canvas_width: 453,
+		canvas_height: 66,
+		bar_width: 3,
+		bar_gap : 0.2,
+		wave_color: "#666",
+		download: false,
+		onComplete: function(png, pixels) {}
 	},
 
-	generate: function(file, download, bar_width, bar_gap, wave_color, canvas_width, canvas_height, returnFunction) {
+	audioContext: new AudioContext(),
+
+	generate: function(file, options) {
 
 		// preparing canvas
 		this.settings.canvas = document.createElement('canvas');
 		this.settings.context = this.settings.canvas.getContext('2d');
 
-		this.settings.canvas.width = (canvas_width !== undefined) ? parseInt(canvas_width) : 453;
-		this.settings.canvas.height = (canvas_height !== undefined) ? parseInt(canvas_height) : 66;
+		this.settings.canvas.width = (options.canvas_width !== undefined) ? parseInt(options.canvas_width) : this.settings.canvas_width;
+		this.settings.canvas.height = (options.canvas_height !== undefined) ? parseInt(options.canvas_height) : this.settings.canvas_height;
 
 		// setting fill color
-		this.settings.wave.color = (wave_color !== undefined) ? wave_color : this.settings.wave.color;
+		this.settings.wave_color = (options.wave_color !== undefined) ? options.wave_color : this.settings.wave_color;
 
 		// setting bars width and gap
-		this.settings.bar.width = (bar_width !== undefined) ? parseInt(bar_width) : this.settings.bar.width;
-		this.settings.bar.gap = (bar_gap !== undefined) ? parseFloat(bar_gap) : this.settings.bar.gap;
+		this.settings.bar_width = (options.bar_width !== undefined) ? parseInt(options.bar_width) : this.settings.bar_width;
+		this.settings.bar_gap = (options.bar_gap !== undefined) ? parseFloat(options.bar_gap) : this.settings.bar_gap;
 
-		this.settings.returnFunction = returnFunction;
+		this.settings.download = (options.download !== undefined) ? options.download : this.settings.download;
 
-		this.settings.download = (download !== undefined) ? download : this.settings.download;
+		this.settings.onComplete = (options.onComplete !== undefined) ? options.onComplete : this.settings.onComplete;
 
 		// read file buffer
 		var reader = new FileReader();
         reader.onload = function(event) {
-            SoundCloudWaveform.settings.audioContext.decodeAudioData(event.target.result, function(buffer) {
+            SoundCloudWaveform.audioContext.decodeAudioData(event.target.result, function(buffer) {
                 SoundCloudWaveform.extractBuffer(buffer);
             });
         };
@@ -54,11 +54,11 @@ var SoundCloudWaveform = {
 	    var len = Math.floor(buffer.length / sections);
 	    var maxHeight = this.settings.canvas.height;
 	    var vals = [];
-	    for (var i = 0; i < sections; i += this.settings.bar.width) {
+	    for (var i = 0; i < sections; i += this.settings.bar_width) {
 	        vals.push(this.bufferMeasure(i * len, len, buffer) * 10000);
 	    }
 
-	    for (var j = 0; j < sections; j += this.settings.bar.width) {
+	    for (var j = 0; j < sections; j += this.settings.bar_width) {
 	        var scale = maxHeight / vals.max();
 	        var val = this.bufferMeasure(j * len, len, buffer) * 10000;
 	        val *= scale;
@@ -69,7 +69,7 @@ var SoundCloudWaveform = {
 	    if (this.settings.download) {
 	    	this.generateImage();
 	    }
-	    this.settings.returnFunction(this.settings.canvas.toDataURL('image/png'), this.settings.context.getImageData(0, 0, this.settings.canvas.width, this.settings.canvas.height));
+	    this.settings.onComplete(this.settings.canvas.toDataURL('image/png'), this.settings.context.getImageData(0, 0, this.settings.canvas.width, this.settings.canvas.height));
     },
 
     bufferMeasure: function(position, length, data) {
@@ -82,11 +82,11 @@ var SoundCloudWaveform = {
 
     drawBar: function(i, h) {
 
-    	this.settings.context.fillStyle = this.settings.wave.color;
+    	this.settings.context.fillStyle = this.settings.wave_color;
 
-		var w = this.settings.bar.width;
-        if (this.settings.bar.gap !== 0) {
-            w *= Math.abs(1 - this.settings.bar.gap);
+		var w = this.settings.bar_width;
+        if (this.settings.bar_gap !== 0) {
+            w *= Math.abs(1 - this.settings.bar_gap);
         }
         var x = i + (w / 2),
             y = this.settings.canvas.height - h;
